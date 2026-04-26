b5e7e9de7698feb44d50b9019bfd1a4502bae1ab
Parham <prmbas@gmail.com>
2026-04-26 19:16:10 +0000

    some prints added

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -15,7 +15,7 @@ import (
 func main() {
 	cliargs := ct.CLIArgs{}
 	cliargs.Init()
-	imageName := (*cliargs.ImageName) // "docker.iranserver.com/alpine:latest"
+	imageName := (*cliargs.ImageName)
 	dirName := "IMG_" + strings.Map(func(r rune) rune {
 		if r == '.' || r == '/' || r == ':' {
 			return '_'
@@ -23,51 +23,57 @@ func main() {
 		return r
 	}, imageName)
 	imageTar := dirName + ".tar"
+	// Print some stuff
+	fmt.Println("[*] Processing Image: ", imageName)
 	cli, err := dt.CreateClient()
 	if err != nil {
-		fmt.Println()
+		fmt.Println("main.go:", err)
 		return
 	}
 	if err := dt.SaveImageToTar(cli, imageName, imageTar); err != nil {
-		fmt.Println(err)
+		fmt.Println("main.go:", err)
 		return
 	}
+	fmt.Println("	-> Tar file: ", imageTar)
 	if !(*cliargs.Keep){
 		defer os.Remove(imageTar)
 	}
 	if err := tt.UnTar(imageTar, dirName); err != nil {
-		fmt.Println(err)
+		fmt.Println("main.go:", err)
 		return
 	}
+	fmt.Println("	-> Contents dir: ", dirName)
 	var man []dt.Manifest
+	fmt.Println("[*] Processing Layers")
 	if err := dt.ReadManifest(&man, path.Join(dirName,"manifest.json")); err != nil {
-		fmt.Printf("%w", err)
+		fmt.Println("main.go:", err)
 		return
 	}
 	// fmt.Println(man)
 	// fmt.Println(len(man[0].Layers))
 	for i, layerSHA256Name := range man[0].Layers {
-		folderName := path.Join(dirName, "LAYER_" + strconv.Itoa(i))
+		folderName := path.Join(dirName, "LAYER_" + strconv.Itoa(i+1))
 		layerFile := path.Join(dirName, layerSHA256Name)
-		fmt.Println(folderName, layerSHA256Name)
+		// fmt.Println(folderName, layerSHA256Name)
+		fmt.Printf("	-> Layer %3d of %3d @ %s\n", i+1, len(man[0].Layers), folderName)
 		if err := tt.UnTar(layerFile, folderName); err != nil {
-			fmt.Println("Error in deep un-tar", err)
+			fmt.Println("main.go: error in deep un-tar", err)
 			return
 		}
 		if err := os.Remove(layerFile); err != nil {
-			fmt.Println("Error in deep un-tar - Removal", err)
+			fmt.Println("main.go: error in deep un-tar - Removal", err)
 			return
 		}
 	}
 	iotools.SafeMkdir(path.Join(dirName, "metadata"))
 	files, _ := os.ReadDir(path.Join(dirName, "blobs/sha256"))
 	for _, i := range files {
-		fmt.Println(i.Name())
+		//fmt.Println(i.Name())
 		os.Rename(
 			path.Join(dirName, "blobs/sha256", i.Name()), 
 			path.Join(dirName, "metadata", i.Name()),
 		)
 	}
 	os.RemoveAll(path.Join(dirName, "blobs"))
-	fmt.Println("Decomposed finished, data available in: ", dirName)
+	//fmt.Println("Decomposed finished, data available in: ", dirName)
 }
