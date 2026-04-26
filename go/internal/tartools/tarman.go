efce38e520f5ed5df0f7c98dd891cc93bbdcfd6f
Parham <prmbas@gmail.com>
2026-04-26 16:36:04 +0000

    tarman: error texts added

--- go/internal/tartools/tarman.go
+++ go/internal/tartools/tarman.go
@@ -9,13 +9,13 @@ import (
 
 func UnTar(filename, dst string) error {
 	if err := iotools.SafeMkdir(dst); err != nil {
-		return fmt.Errorf("%w", err)
+		return fmt.Errorf("tarman.go: UnTar(...): Create directory stage %w", err)
 	}
 	cmd := exec.Command("tar", "-xvf", filename, "-C", dst)
 	/*cmd.StdOut = os.StdOut
 	cmd.stderr = os.stderr*/
 	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("%w", err)
+		return fmt.Errorf("tarman.go: UnTar(...): Command run stage %w", err)
 	}
 	return nil
 }
