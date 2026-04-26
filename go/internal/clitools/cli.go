2538694954dca2ca1ac7a82cd680c3191494872b
Parham <prmbas@gmail.com>
2026-04-26 19:06:18 +0000

    panic if no image name passed as cli arg

--- go/internal/clitools/cli.go
+++ go/internal/clitools/cli.go
@@ -13,4 +13,7 @@ func (c *CLIArgs) Init() {
 	c.ImageName = flag.String("i", "", "Name of the image to decompose")
 	c.Keep = flag.Bool("k", false, "Keep image tar file")
 	flag.Parse()
+	if (*c.ImageName) == "" {
+		panic("No image name provided. Use flag `-i` to pass image name.")
+	}
 }
