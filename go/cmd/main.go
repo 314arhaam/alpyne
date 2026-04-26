fae9b578373472cca40e7de880cb5be7baef012a
Parham <prmbas@gmail.com>
2026-04-26 17:30:32 +0000

    directory name is now image name with . / : replaced by _

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -7,32 +7,40 @@ import (
 	"fmt"
 	"strconv"
 	"os"
+	"strings"
 )
 
 func main() {
+	imageName := "docker.iranserver.com/alpine:latest"
+	dirName := strings.Map(func(r rune) rune {
+		if r == '.' || r == '/' || r == ':' {
+			return '_'
+		}
+		return r
+	}, imageName)
 	cli, err := dt.CreateClient()
 	if err != nil {
 		fmt.Println()
 		return
 	}
-	if err := dt.SaveImageToTar(cli, "docker.iranserver.com/alpine:latest", "alp.tar"); err != nil {
+	if err := dt.SaveImageToTar(cli, imageName, "alp.tar"); err != nil {
 		fmt.Println(err)
 		return
 	}
-	if err := tt.UnTar("alp.tar", "image_data"); err != nil {
+	if err := tt.UnTar("alp.tar", dirName); err != nil {
 		fmt.Println(err)
 		return
 	}
 	var man []dt.Manifest
-	if err := dt.ReadManifest(&man, "image_data/manifest.json"); err != nil {
+	if err := dt.ReadManifest(&man, dirName + "/manifest.json"); err != nil {
 		fmt.Printf("%w", err)
 		return
 	}
 	fmt.Println(man)
 	fmt.Println(len(man[0].Layers))
 	for i, f := range man[0].Layers {
-		folderName := "image_data/LAYER_" + strconv.Itoa(i)
-		layerFile := "image_data/" + f
+		folderName := dirName + "/LAYER_" + strconv.Itoa(i)
+		layerFile := dirName + "/" + f
 		fmt.Println(folderName, f)
 		if err := tt.UnTar(layerFile, folderName); err != nil {
 			fmt.Println("Error in deep un-tar", err)
@@ -43,11 +51,11 @@ func main() {
 			return
 		}
 	}
-	iotools.SafeMkdir("image_data/metadata")
-	a, _ := os.ReadDir("image_data/blobs/sha256")
+	iotools.SafeMkdir(dirName + "/metadata")
+	a, _ := os.ReadDir(dirName + "/blobs/sha256")
 	for _, i := range a {
 		fmt.Println(i.Name())
-		os.Rename("image_data/blobs/sha256/" + i.Name(), "image_data/metadata/" + i.Name())
+		os.Rename(dirName + "/blobs/sha256/" + i.Name(), dirName + "/metadata/" + i.Name())
 	}
-	os.RemoveAll("image_data/blobs")
+	os.RemoveAll(dirName + "/blobs")
 }
